a78e7c8a38b250e9dfe9a3fb837db69b98c1d1de
Jessica Alan <JessicaAlan440@gmail.com>
2018-04-16 13:10:57 -0500

    wrote test script

--- Brainduino_test/Brainduino_test.ino
+++ Brainduino_test/Brainduino_test.ino
@@ -1,9 +1,17 @@
+int LEDpin, rx, tx;
+
 void setup() {
   // put your setup code here, to run once:
-
+  LEDpin = 8;
+  rx = 0;
+  tx = 1; 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (digitalRead(rx) == HIGH) {
+    digitalWrite(LEDpin, HIGH);
+  } else {
+    digitalWrite(LEDpin, LOW);
+  }
 }
